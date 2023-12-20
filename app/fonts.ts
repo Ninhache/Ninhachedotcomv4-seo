0886c8ff542d2d1b6be3b691f00a1f65559d68c5
Ninhache <mr.carambar27@gmail.com>
2023-12-20 08:52:03 +0100

    feat: export file to handle all fonts (and to cache them according to the docs)

---
+++ app/fonts.ts
@@ -0,0 +1,23 @@
+import localFont from 'next/font/local'
+
+export const calibreSemibold = localFont({
+    src: '../fonts/calibre/CalibreSemibold.otf',
+    display: 'swap'
+})
+
+export const calibreRegular = localFont({
+    src: '../fonts/calibre/CalibreRegular.otf',
+    display: 'swap'
+})
+
+export const ralewaySemiBold = localFont({
+    src: '../fonts/raleway/Raleway-SemiBold.ttf',
+    display: 'swap'
+})
+
+
+export const proximaNovaBold = localFont({
+    src: '../fonts/proxima_nova/ProximaNovaBold.otf',
+    display: 'swap'
+})
+
