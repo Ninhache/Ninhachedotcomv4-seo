0e1a639f8bf3040dc4e875d6d81962cf9ede4fc6
Ninhache <mr.carambar27@gmail.com>
2023-12-28 06:24:56 +0100

    fix: remove useless viewport meta tag since it's handled differently in NextJS

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,6 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     site: "@NinhacheUwU",
   },
-  viewport: "width=device-width, initial-scale=1",
 }
 
 export default function RootLayout({ children}: {
