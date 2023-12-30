d9e88c64e5118d595a32d495b3b380f70db21013
Ninhache <mr.carambar27@gmail.com>
2023-12-30 11:16:23 +0100

    fix: refacto + make the component works with new styling

---
+++ components/experience/ExperienceItem.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { calibreRegular, calibreSemibold, ralewayMedium, ralewaySemiBold } from '@/app/fonts';
+import { Experience } from '@/jsons/jsonUtils';
+import styles from '@/styles/experience/experienceItem.module.css';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+interface ExperienceItemProps {
+	inverted: Boolean;
+	experience: Experience;
+}
+
+export const ExperienceItem: React.FC<ExperienceItemProps> = ({ inverted, experience }) => {
+
+	const parts = experience.date.split("<br>");
+
+	return (
+		<div className={`${styles.content} ${inverted ? styles.inverted : ""}`}>
+			<div className={`${styles.information}`}>
+				<div className={`${styles.type}`}
+				style={{
+					marginLeft: inverted ? 'unset' : '15px'
+				}}
+				>
+					<span 
+						className={`${ralewayMedium.className}`}
+						style={{
+							marginRight: inverted ? '15px' : 'unset',
+						}}
+					>
+						{parts.map((part, index) => (
+							<Fragment key={index}>
+								{part}
+								{index !== parts.length - 1 && <br />}
+							</Fragment>
+						))}
+					</span>
+					{
+						experience.type && <span className={`${styles.type} ${ralewayMedium.className}`}><i>{experience.type}</i></span>
+					}
+				</div>
+				<a className={`${styles.title} ${calibreSemibold.className}`} href={`${experience.link}`} target="_blank">
+					{experience.title}
+				</a>
+				<div className={`${styles.text} ${calibreRegular.className}`}>
+					<p>{experience.description}</p>
+					</div>
+				<div className={`${styles.tags}`}>
+					{
+						experience.tags.map(tag => <Link className={`${styles.tag} ${ralewaySemiBold.className}`} key={tag.name} href={`${tag.url}`} target="_blank">{tag.name}</Link>)
+					}
+				</div>
+			</div>
+			<div className={`${styles.view}`}>
+				<a className={`${styles.website_link}`} href={`${experience.link}`} target="_blank">
+					<img className={`${styles.image}`} src={`${experience.image}`} />
+					{experience.videoUrl !== `none` &&
+						(<video className={`${styles.video}`} loop muted preload={`metadata`}>
+							<source src={`${experience.videoUrl}`} type="video/mp4" />
+						</video>)}
+				</a>
+			</div>
+		</div>
+	)
+};
---
+++ components/experience/experience.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { proximaNovaBold } from '@/app/fonts';
+
+import rawData from '@/jsons/experiences.json';
+import AnimatedComponent from '../AnimatedComponent';
+
+import { ExperienceItem } from './ExperienceItem';
+
+import styles from '@/styles/experience/experience.module.css';
+
+export default function Experience() {
+
+	return (
+		<>
+			<section className={`section ${styles.section}`} id="experience">
+				<div className={`content leaning`}>
+					<span className={`section_title ${styles.title} ${proximaNovaBold.className}`}>Where I&apos;ve Worked</span>
+					<div className={styles.content}>
+						{
+							rawData.map((item, index) => {
+								return (
+									<AnimatedComponent delay={100} key={index}>
+										<ExperienceItem key={index} experience={item} inverted={index % 2 === 0} />
+									</AnimatedComponent>
+								)
+							})
+						}
+					</div>
+				</div>
+			</section>
+			<div className={`section_end ${styles.experience_end}`} />
+		</>
+	)
+}
