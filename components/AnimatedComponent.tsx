11d0989fa784f39780d6a7b6a6f4ac858f99a670
Ninhache <mr.carambar27@gmail.com>
2023-12-30 11:31:32 +0100

    feat: add a way to add additionnal CSS

--- components/AnimatedComponent.tsx
+++ components/AnimatedComponent.tsx
@@ -1,14 +1,14 @@
-// AnimatedComponent.tsx
-import React, { useEffect, useState } from 'react';
+import React, { CSSProperties, useEffect, useState } from 'react';
 import { useIntersectionObserver } from './hooks/useIntersectionObserver';
 
 interface AnimatedComponentProps {
     children: React.ReactNode;
     delay?: number;
     translateY?: string;
+    customCss?: CSSProperties
 }
 
-const AnimatedComponent: React.FC<AnimatedComponentProps> = ({ children, delay = 0, translateY = '30px' }) => {
+const AnimatedComponent: React.FC<AnimatedComponentProps> = ({ children, delay = 0, translateY = '30px', customCss = {} }) => {
     const [isVisible, ref] = useIntersectionObserver();
     const [shouldAnimate, setShouldAnimate] = useState(false);
 
@@ -25,7 +25,7 @@ const AnimatedComponent: React.FC<AnimatedComponentProps> = ({ children, delay =
             opacity: shouldAnimate ? 1 : 0,
             transform: shouldAnimate ? 'translateY(0)' : `translateY(30px)`,
             transition: 'opacity 0.3s ease, transform 0.3s ease',
-            // Add any additional styles you need here
+            ...customCss
         }}>
             {children}
         </div>
