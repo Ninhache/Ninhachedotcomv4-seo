104b3945179be5670d2743850d0acaf2c63a48a1
Ninhache <mr.carambar27@gmail.com>
2024-01-30 00:12:10 +0100

    fix: moved files.

---
+++ app/_hooks/useLocaleNames.tsx
@@ -0,0 +1,14 @@
+import { Locale, locales } from "@/config";
+import { useTranslations } from "next-intl";
+
+export default function useLocaleNames(): Record<Locale, string> {
+  const t = useTranslations("useLocaleNames");
+
+  return locales.reduce(
+    (acc, locale) => {
+      acc[locale] = t(locale) as string;
+      return acc;
+    },
+    {} as Record<Locale, string>,
+  );
+}
