51339f0c528671152aad961704dc2665b75ce5b3
Ninhache <mr.carambar27@gmail.com>
2024-04-06 22:53:10 +0200

    feat: add /resume as route to ease resume sharing

---
+++ app/[locale]/resume/page.tsx
@@ -0,0 +1,16 @@
+import { defaultLocale } from '@/config';
+import { unstable_setRequestLocale } from 'next-intl/server'
+import {redirect} from 'next/navigation';
+
+type Props = {
+  params: { locale: string };
+};
+
+export default async function Page({ params: { locale } }: Props) {
+
+  unstable_setRequestLocale(locale);
+
+  await fetch(`http://localhost:3000/documents/CV_ALMEIDA_Neo_${locale.toLocaleUpperCase()}-web.pdf`)
+          .then(() => redirect(`/documents/CV_ALMEIDA_Neo_${locale.toLocaleUpperCase()}-web.pdf`))
+          .catch(e => redirect(`/documents/CV_ALMEIDA_Neo_${defaultLocale}-web.pdf`));
+}
