6e5246aa6b919cb66954acc8c8537cc9847e855a
Ninhache <mr.carambar27@gmail.com>
2026-06-02 16:52:45 +0200

    chore: upgrade to next 16.2.7 and drop stale tooling

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const config = {
     // Pin the workspace root: a stray lockfile in a parent dir made Turbopack
     // infer the wrong root. Without this, Next warns and may resolve from there.
     turbopack: { root: __dirname },
-    webpack: config => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
-
-        return config;
-    },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
